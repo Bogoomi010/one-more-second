4c697558f2f53e0633ed8508111e01847ed6a40e
kbk5675 <kbk980130@gamil.com>
2026-02-25 21:34:25 +0900

    Move header subtitle upward with translate

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -328,7 +328,7 @@ export default function Layout({
               SECOND
             </span>
           </button>
-          <span className="text-brand-subtitle sm:text-brand-subtitle-lg leading-none text-text-muted font-primary self-end -mt-1">
+          <span className="text-brand-subtitle sm:text-brand-subtitle-lg leading-none text-text-muted font-primary self-end -translate-y-2">
             dodge game
           </span>
         </div>
