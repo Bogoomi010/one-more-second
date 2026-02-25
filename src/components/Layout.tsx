4c343c491e8d67dcfa4acee20edcdae51d0ebfce
kbk5675 <kbk980130@gamil.com>
2026-02-25 21:34:05 +0900

    Raise header subtitle further

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -328,7 +328,7 @@ export default function Layout({
               SECOND
             </span>
           </button>
-          <span className="text-brand-subtitle sm:text-brand-subtitle-lg leading-none text-text-muted font-primary self-end -mt-0.5">
+          <span className="text-brand-subtitle sm:text-brand-subtitle-lg leading-none text-text-muted font-primary self-end -mt-1">
             dodge game
           </span>
         </div>
